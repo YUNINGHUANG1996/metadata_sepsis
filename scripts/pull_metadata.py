7e8e8c3a33198c9f35023d78f850163834356c79
YUNINGHUANG1996 <76122438+YUNINGHUANG1996@users.noreply.github.com>
2025-06-22 11:43:10 -0700

    Update pull_metadata.py

--- scripts/pull_metadata.py
+++ scripts/pull_metadata.py
@@ -2,74 +2,77 @@ import pandas as pd
 import xmltodict
 import os
 
-
 def parse_xml(filepath):
     with open(filepath) as fd:
         data_dict = xmltodict.parse(fd.read())
 
     accession = filepath.split('/')[-1].split('_')[0]
-    print(accession)
-    print('\n')
-    r_df = pd.DataFrame()
+    print(accession + '\n')
+    rows = []
 
     for sample in data_dict['MINiML']['Sample']:
-        tmp_df = {}
+        tmp = {}
 
         if 'Status' in sample:
-            tmp_df['database'] = [sample['Status']['@database']]
-            tmp_df['submission_date'] = [sample['Status']['Submission-Date']]
-            tmp_df['release_date'] = [sample['Status']['Release-Date']]
-            tmp_df['last_update_date'] = [sample['Status']['Last-Update-Date']]
-        tmp_df['title'] = [sample['Title']]
-        tmp_df['accession'] = [sample['Accession']['#text']]
-        tmp_df['type'] = [sample['Type']]
-        tmp_df['source'] = [sample['Channel']['Source']]
-        tmp_df['organism'] = [sample['Channel']['Organism']['#text']]
+            tmp['database'] = sample['Status'].get('@database', '')
+            tmp['submission_date'] = sample['Status'].get('Submission-Date', '')
+            tmp['release_date'] = sample['Status'].get('Release-Date', '')
+            tmp['last_update_date'] = sample['Status'].get('Last-Update-Date', '')
 
-        if isinstance(sample['Channel']['Characteristics'], list):
-            count = 0
-            for c in sample['Channel']['Characteristics']:
-                if '@tag' in c:
-                    if '#text' in c:
-                        tmp_df[c['@tag']] = [c['#text']]
-                    else:
-                        tmp_df[c['@tag']] = ['']
-                else:
-                    tmp_df['characteristic_' + str(count)] = [c]
-                count += 1
-        elif isinstance(sample['Channel']['Characteristics'], dict):
-            if '@tag' in sample['Channel']['Characteristics']:
-                tmp_df[sample['Channel']['Characteristics']['@tag']] = [sample['Channel']['Characteristics']['#text']]
+        tmp['title'] = sample.get('Title', '')
+        tmp['accession'] = sample['Accession'].get('#text', sample['Accession'])
+        tmp['type'] = sample.get('Type', '')
+        tmp['source'] = sample['Channel'].get('Source', '')
+        tmp['organism'] = sample['Channel']['Organism'].get('#text', sample['Channel']['Organism'])
+
+        characs = sample['Channel'].get('Characteristics', [])
+        if isinstance(characs, dict):
+            characs = [characs]
+        for i, c in enumerate(characs):
+            if isinstance(c, dict):
+                tag = c.get('@tag', f'characteristic_{i}')
+                value = c.get('#text', '')
             else:
-                tmp_df['characteristic'] = [sample['Channel']['Characteristics']]
-        else:
-            tmp_df['characteristic'] = [sample['Channel']['Characteristics']]
+                tag, value = f'characteristic_{i}', c
+            tmp[tag] = value
 
-        if 'Treatment-Protocol' in sample['Channel']:
-            tmp_df['treatment_protocol'] = [sample['Channel']['Treatment-Protocol']]
-        tmp_df['molecule'] = [sample['Channel']['Molecule']]
-        if 'Extract-Protocol' in sample['Channel']:
-            tmp_df['extract_protocol'] = [sample['Channel']['Extract-Protocol']]
-        if 'Label' in sample['Channel']:
-            tmp_df['label'] = [sample['Channel']['Label']]
-        if 'Label-Protocol' in sample['Channel']:
-            tmp_df['label_protocol'] = [sample['Channel']['Label-Protocol']]
-        if 'Hybridization-Protocol' in sample:
-            tmp_df['hybridization_protocol'] = [sample['Hybridization-Protocol']]
-        if 'Scan-Protocol' in sample:
-            tmp_df['scan_protocol'] = [sample['Scan-Protocol']]
-        if 'Description' in sample:
-            tmp_df['description'] = [sample['Description']]
-        if 'Data-Processing' in sample:
-            tmp_df['data_processing'] = [sample['Data-Processing']]
-        r_df = r_df.append(pd.DataFrame.from_dict(tmp_df), ignore_index=True, sort=False)
-    if os.path.exists('../summary_data'):
-        r_df.to_csv('../summary_data/' + accession + '_metadata.csv', index=False)
-    else:
-        os.mkdir('../summary_data')
-        r_df.to_csv('../summary_data/' + accession + '_metadata.csv', index=False)
+        tmp['treatment_protocol'] = sample['Channel'].get('Treatment-Protocol', '')
+        tmp['molecule'] = sample['Channel'].get('Molecule', '')
+        tmp['extract_protocol'] = sample['Channel'].get('Extract-Protocol', '')
+        tmp['label'] = sample['Channel'].get('Label', '')
+        tmp['label_protocol'] = sample['Channel'].get('Label-Protocol', '')
+        tmp['hybridization_protocol'] = sample.get('Hybridization-Protocol', '')
+        tmp['scan_protocol'] = sample.get('Scan-Protocol', '')
+        tmp['description'] = sample.get('Description', '')
+        tmp['data_processing'] = sample.get('Data-Processing', '')
 
+        rows.append(tmp)
+
+    df = pd.DataFrame(rows)
+    os.makedirs('../summary_data', exist_ok=True)
+    df.to_csv(f'../summary_data/{accession}_metadata.csv', index=False)
 
 if __name__ == '__main__':
     for file in os.listdir('../raw_data'):
-        parse_xml('../raw_data/' + file)
+        if file.endswith('.xml'):
+            parse_xml(os.path.join('../raw_data', file))
+
+
+
+def combine_csvs(directory='../summary_data', key_column='accession'):
+    combined_df = pd.DataFrame()
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.csv'):
+            filepath = os.path.join(directory, filename)
+            df = pd.read_csv(filepath)
+            if key_column in df.columns:
+                combined_df = pd.concat([combined_df, df], ignore_index=True)
+            else:
+                print(f"Skipping {filename}: missing '{key_column}' column")
+
+    combined_df.to_csv(os.path.join(directory, 'combined_metadata.csv'), index=False)
+    print(f"Combined {len(combined_df)} rows into 'combined_metadata.csv'")
+
+if __name__ == '__main__':
+    combine_csvs()
