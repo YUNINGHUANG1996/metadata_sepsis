c0b72c263a990f2fb0a6fcacdab47e4bf276c311
YUNINGHUANG1996 <76122438+YUNINGHUANG1996@users.noreply.github.com>
2025-06-22 11:42:24 -0700

    Update get_xml_files.py

--- scripts/get_xml_files.py
+++ scripts/get_xml_files.py
@@ -2,27 +2,45 @@ from ftplib import FTP
 import os
 import tarfile
 
-
-def get_xml(accession, ftp):
+def get_xml(accession: str):
     acc_dir = accession[:-3] + 'nnn'
-    filename = accession + '_family.xml'
-    url = '/geo/series/' + acc_dir + '/' + accession + '/miniml/' + filename + '.tgz'
-    with open(filename + '.tgz', 'wb') as fp:
-        ftp.retrbinary('RETR ' + url, fp.write)
-
-    with tarfile.open(filename + '.tgz', mode='r:gz') as tf:
-        tf.extract(filename, path='../raw_data/')
-
-    os.remove(filename + '.tgz')
-
-
-if __name__ == '__main__':
-    if not os.path.exists('../raw_data'):
-        os.mkdir('../raw_data')
+    tgz_filename = accession + '_family.xml.tgz'
+    ftp_path = f'/geo/series/{acc_dir}/{accession}/miniml/{tgz_filename}'
 
     ftp = FTP('ftp.ncbi.nlm.nih.gov')
     ftp.login()
-    with open('accession_list.txt', 'r') as acc_file:
-        for accession in acc_file.readlines():
-            get_xml(accession.rstrip(), ftp)
+
+    try:
+        with open(tgz_filename, 'wb') as fp:
+            ftp.retrbinary(f'RETR {ftp_path}', fp.write)
+    except Exception as e:
+        ftp.quit()
+        raise RuntimeError(f"FTP download failed for {accession}: {e}")
+
     ftp.quit()
+
+    try:
+        with tarfile.open(tgz_filename, mode='r:gz') as tf:
+            xml_members = [m for m in tf.getnames() if m.endswith('.xml')]
+            if xml_members:
+                for member in xml_members:
+                    tf.extract(member, path='../raw_data/')
+            else:
+                raise FileNotFoundError(f"No .xml file found in archive: {tgz_filename}. Members: {tf.getnames()}")
+    finally:
+        os.remove(tgz_filename)
+
+def main():
+    os.makedirs('../raw_data', exist_ok=True)
+
+    with open('accession_list.txt', 'r') as acc_file:
+        for accession in acc_file:
+            accession = accession.strip()
+            if accession:
+                try:
+                    get_xml(accession)
+                except Exception as e:
+                    print(f"Error processing {accession}: {e}")
+
+if __name__ == '__main__':
+    main()
